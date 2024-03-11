9524d8777cfa69ed8485afce6fb0b8566cc2c919
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2024-03-11 05:02:56 +0100

    added

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from datetime import datetime
 
 def home(request):
-    return HttpResponse("Hello, Writing my first Django code.")
+    return HttpResponse("Welcome to little lemon restaurant!")
+
+def display_date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
 
 
