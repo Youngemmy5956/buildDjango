6a130f699d1206468bc874dab828f09ecbad23eb
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2024-03-19 20:49:45 +0100

    added

--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from http.client import HTTPResponse
 from django.http import HttpResponse
 from datetime import datetime
 
@@ -12,3 +12,11 @@ def display_date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
 
 
+def drinks(request, drink_name):
+    drink = {
+        'mocha' : 'type of coffee',
+        'tea' : 'type of hot beverage',
+        'lemonade': 'type of refreshment'
+    }
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
