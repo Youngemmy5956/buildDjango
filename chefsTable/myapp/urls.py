a42bf87458e6cdb7e14be9c5e163de481b9e03f7
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2024-03-21 03:19:09 +0100

    added

--- chefsTable/myapp/urls.py
+++ chefsTable/myapp/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     path('', views.home),
      path('drinks/<str:drink_name>', views.drinks, name="drink_name"), 
      path('date', views.display_date),
-     path('foods/<str:food_name>', views.foods, name="food_name")
+     path('foods/<str:food_name>', views.foods, name="food_name"),
+      path('', views.home, name="home"),
+        path('menu/', views.menu, name="menu"),
+        path('about/', views.about, name="about"),
+        path('book/', views.book, name="book"),
      
 ]
--- chefsTable/myapp/views.py
+++ chefsTable/myapp/views.py
@@ -29,3 +29,17 @@ def foods(request, food_name):
     }
     choice_of_food = food[food_name]
     return HttpResponse(f"<h2>{food_name}</h2> " + choice_of_food)
+
+
+def home(request):
+    return HttpResponse("Welcome to Little Lemon !")
+
+def about(request):
+    return HttpResponse("About us")
+
+def menu(request):
+    return HttpResponse("Menu for Little Lemon")
+
+def book(request):
+    return HttpResponse("Make a booking")
+
