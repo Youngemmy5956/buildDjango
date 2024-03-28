614a40fc19ae24491b789d0046fbea50029e833f
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2024-03-28 04:11:34 +0100

    updated

--- chefsTable/myapp/admin.py
+++ chefsTable/myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Drinks
+from .models import MenuCategory
+from .models import Menu
+
 # Register your models here.
+admin.site.register(Drinks)
+admin.site.register(MenuCategory)
+admin.site.register(Menu)
--- chefsTable/myapp/models.py
+++ chefsTable/myapp/models.py
@@ -9,3 +9,21 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.name + ' : ' + self.cuisine 
+    
+    
+class Drinks(models.Model):
+    drink = models.CharField(max_length=200)
+    price = models.IntegerField()
+    
+    class Drinks(models.Model):
+          drink_name = models.CharField(max_length=200)
+          price = models.IntegerField()
+          
+          
+    class MenuCategory(models.Model):
+          category_name = models.CharField(max_length=200)
+          
+    class Menu(models.Model):
+          menu_item = models.CharField(max_length=200)
+          price = models.IntegerField(null=False)
+          category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
