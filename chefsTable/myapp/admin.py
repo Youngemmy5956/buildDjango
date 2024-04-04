d2fe5f196c19429af4f777ec46499918cbd3ca21
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2024-04-04 03:57:07 +0100

    updated learning

--- chefsTable/myapp/admin.py
+++ chefsTable/myapp/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Drinks
+# from .models import Drinks
 from .models import MenuCategory
 from .models import Menu
+from .models import Drinks
+from .models import DrinksCategory
 
 # Register your models here.
-admin.site.register(Drinks)
+# admin.site.register(Drinks)
 admin.site.register(MenuCategory)
 admin.site.register(Menu)
+admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
--- chefsTable/myapp/models.py
+++ chefsTable/myapp/models.py
@@ -1,29 +1,38 @@
 from django.db import models
 
 # Create your models here.
-class Menu(models.Model):
-    name = models.CharField(max_length=100)
-    cuisine = models.CharField(max_length=100)
-    price = models.IntegerField()
+# class Menu(models.Model):
+#     name = models.CharField(max_length=100)
+#     cuisine = models.CharField(max_length=100)
+#     price = models.IntegerField()
     # image = models.ImageField(upload_to='menu_images')
 
-    def __str__(self):
-        return self.name + ' : ' + self.cuisine 
+    # def __str__(self):
+    #     return self.name + ' : ' + self.cuisine 
     
     
+# class Drinks(models.Model):
+#     drink = models.CharField(max_length=200)
+#     price = models.IntegerField()
+    
+# class Drinks(models.Model):
+#     drink_name = models.CharField(max_length=200)
+#     price = models.IntegerField()
+          
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
+    
+    
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+
 class Drinks(models.Model):
     drink = models.CharField(max_length=200)
     price = models.IntegerField()
+    category_id = models.ForeignKey(DrinksCategory, on_delete=models.PROTECT, default=None)
     
-    class Drinks(models.Model):
-          drink_name = models.CharField(max_length=200)
-          price = models.IntegerField()
-          
-          
-    class MenuCategory(models.Model):
-          category_name = models.CharField(max_length=200)
-          
-    class Menu(models.Model):
-          menu_item = models.CharField(max_length=200)
-          price = models.IntegerField(null=False)
-          category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
