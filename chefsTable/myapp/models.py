2dfcc1b2ff090b3473205186bb8d107cb52802d7
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2024-04-06 04:06:03 +0100

    updated learning

--- chefsTable/myapp/models.py
+++ chefsTable/myapp/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 # Create your models here.
-# class Menu(models.Model):
-#     name = models.CharField(max_length=100)
-#     cuisine = models.CharField(max_length=100)
-#     price = models.IntegerField()
-    # image = models.ImageField(upload_to='menu_images')
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='menu_images')
 
-    # def __str__(self):
-    #     return self.name + ' : ' + self.cuisine 
+    def __str__(self):
+        return self.name + ' : ' + self.cuisine 
     
     
 class Drinks(models.Model):
